4a4b72b6688d1619feab00f25047da0c97835764
Kavya Sahai <kavyamohan.sahai@sjsu.edu>
2019-02-26 20:23:27 -0800

    Lab5

--- plots.py
+++ plots.py
@@ -2,6 +2,8 @@ from collections import Counter
 from linear_algebra import distance
 from stats import mean
 import math, random
+from knn import knn_classify
+import data
 import matplotlib.pyplot as plt
 
 
@@ -9,7 +11,7 @@ def plot_state_borders(plt, color='0.8'):
     pass
 
 
-def plot_cities(cities):
+def plot_cities(plots,cities):
     # key is language, value is pair (longitudes, latitudes)
     plots = {"Java": ([], []), "Python": ([], []), "R": ([], [])}
 
@@ -34,6 +36,7 @@ def plot_cities(cities):
     plt.show()
 
 
+
 def classify_and_plot_grid(cities, k=1):
     """
     TODO
@@ -48,16 +51,40 @@ def classify_and_plot_grid(cities, k=1):
     # latitude in range (20, 55)
     # Save the coordinate of prediction result in plots variable.
     # TODO
+    for i in range(-130, -59):
+        for j in range(20, 55):
+            pred=knn_classify(k,cities,(i,j))
+            plots[pred][0].append(i)
+            plots[pred][1].append(j)
 
     # create a scatter series for each language
     # See above plot_cities() to plot your prediction.
     # TODO
+    for (longitude, latitude), language in cities:
+        plots[language][0].append(longitude)
+        plots[language][1].append(latitude)
+
+    # create a scatter series for each language
+    for language, (x, y) in plots.items():
+        plt.scatter(x, y, color=colors[language], marker=markers[language],
+                    label=language, zorder=10)
+
+    plot_state_borders(plt)  # assume we have a function that does this
+
+    plt.legend(loc=0)  # let matplotlib choose the location
+    plt.axis([-130, -60, 20, 55])  # set the axes
+    plt.title("Favorite Programming Languages")
+    plt.show()
+
+
+
+
 
 
 if __name__ == "__main__":
     # TODO import cities from data.py
-    cities = []
-    plot_cities(cities)
+    cities = data.cities
+    # plot_cities(cities)
     classify_and_plot_grid(cities)
     classify_and_plot_grid(cities, 3)
     classify_and_plot_grid(cities, 5)
